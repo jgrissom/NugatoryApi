4aefdb587f067e8785cc5812243a6e7b13ee9ee6
jgrissom <jgrissom91@gmail.com>
2024-09-27 23:28:19 -0500

    [1.10] Customize swaggerdoc

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 // Connection info stored in appsettings.json
 IConfiguration configuration = new ConfigurationBuilder()
@@ -14,7 +15,15 @@ builder.Services.AddDbContext<DataContext>(options => options.UseSqlite(configur
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { 
+        Title = "Nugatory", 
+        Version = "v1",
+        Description = "A totally useless API",
+    });
+    c.TagActionsBy(api => [api.HttpMethod]);
+});
 
 var app = builder.Build();
 
