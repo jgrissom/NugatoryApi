d605bfa7fd0f56d5a79041a799c0b344ac603704
jgrissom <jgrissom91@gmail.com>
2024-09-27 23:08:53 -0500

    [1.05] Migrations 🚀 dotnet ef migrations add initSQLite dotnet ef database update

---
+++ Migrations/20240928040618_initSQLite.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NugatoryApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class initSQLite : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Words",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Text = table.Column<string>(type: "TEXT", maxLength: 16, nullable: false),
+                    R = table.Column<int>(type: "INTEGER", nullable: false),
+                    G = table.Column<int>(type: "INTEGER", nullable: false),
+                    B = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Words", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Words");
+        }
+    }
+}
